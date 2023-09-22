208a3f3804d8762834cdef551213bd2a5a8d6eb9
Cervantes Ch. Jhon F <jhonfredycercha@hotmail.com>
2023-09-21 20:43:53 -0500

    Setup project

---
+++ src/main/java/com/unimag/encuestapp/EncuestappApplication.java
@@ -0,0 +1,13 @@
+package com.unimag.encuestapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncuestappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncuestappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unimag/encuestapp/EncuestappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unimag.encuestapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncuestappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
